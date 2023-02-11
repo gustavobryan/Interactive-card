bc3bb66643e197757c47782b63f232ee3276d8fa
GustavoBryan <devgustavob@gmail.com>
2023-02-11 19:16:05 -0300

    🚧 🧱 ci Construção do JavaScript p2

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const cardholder = document.getElementById("cardholder-name");
 const cardNumber = document.getElementById("card-number");
-const expiry = document.getElementById(".expiry");
+const expiry = Array.from(document.querySelectorAll(".expiry"));
 const cvc = document.getElementById("cvc");
 const submit = document.getElementById("submit");
-const nameOnCard = document.getElementById(".cardholder-display");
-const numOnCard = document.getElementById(".card-number-display");
-const expMM = document.getElementById(".expiry-month-display");
-const expYY = document.getElementById(".expiry-year-display");
-const cvcDisplay = document.getElementById(".cvc-display");
+const nameOnCard = document.querySelector(".cardholder-display");
+const numOnCard = document.querySelector(".card-number-display");
+const expMM = document.querySelector(".expiry-month-display");
+const expYY = document.querySelector(".expiry-year-display");
+const cvcDisplay = document.querySelector(".cvc-display");
 const thankYou = document.getElementById("thank-you-header");
-const thankYouSectin = document.getElementById("thank-you");
+const thankYouSection = document.getElementById("thank-you");
 const continueBtn = document.getElementById("continue");
 const form = document.getElementById("myForm");
 const expiryErrorMsg = document.getElementById("expiry-error");
-
 
 //teste;
 
@@ -28,7 +27,7 @@ function inputCardNum() {
     let cardNumberInput = cardNumber.value;
     let formattedCardNumber = cardNumberInput.replace(/[^\d]/g, "");
     formattedCardNumber = formattedCardNumber.substring(0, 16);
-    let cardNumberSections = formattedCardNumber.match(/\s{1,4}/g);
+    let cardNumberSections = formattedCardNumber.match(/\d{1,4}/g);
     if (cardNumberSections !== null) {
         formattedCardNumber = cardNumberSections.join(" ");
     }
@@ -39,7 +38,7 @@ function inputCardNum() {
     if (cardNumber.value === "") {
         numOnCard.innerHTML = cardNumber.placeholder;
     }
-} 
+}
 function inputMM() {
     let formattedMM = expiry[0].value;
     formattedMM = formattedMM.substring(0, 2);
@@ -51,12 +50,122 @@ function inputMM() {
     }
 }
 function inputYY() {
-     let formattedYY = expiry[1].value;
-     formattedYY = formattedYY.substring(0, 4);
-     expiry[1].value = formattedYY;
-     if (expiry[1].value === "") {
+    let formattedYY = expiry[1].value;
+    formattedYY = formattedYY.substring(0, 4);
+    expiry[1].value = formattedYY;
+    if (expiry[1].value === "") {
         expYY.innerHTML = "0000";
-     } else {
+    } else {
         expYY.innerHTML = expiry[1].value;
-     }
+    }
 }
+function inputCvc() {
+    let formattedCvc = cvc.value;
+    
+    formattedCvc = formattedCvc.substring(0, 3);
+    cvc.value = formattedCvc;
+    if (cvc.value === "") {
+        cvcDisplay.innerHTML = "000";
+    } else {
+        cvcDisplay.innerHTML = cvc.value;
+    }
+}
+
+function massValidate() {
+    function validateName() {
+        let cardholderExp = /^[A-Z a-z]+$/;
+        let errorMsg = document.getElementById("msgError");
+        if (cardholder.value.match(cardholderExp)) {
+            errorMsg.textContent = "";
+        } else {
+            errorMsg.innerHTML = "Please enter cardholder name!";
+        }
+    }
+    function validateCard() {
+        let cardNumError = document.getElementById("card-num-error");
+        if (cardNumber.value.length > 0 && cardNumber.value.length < 16) {
+            cardNumError.innerHTML = "Wrong format!";
+        } else if (cardNumber.value == "") {
+            cardNumError.innerHTML = "Can't be blank!";
+        } else {
+            cardNumError.innerHTML = "";
+        }
+    }
+    function validateExpiry() {
+        let expMonth = /^(0[0-9]|1[1-2]){2}$/;
+        let expYear = /^[0-9][0-2]{4}$/;
+
+        if (expiry[0].value.match(expMonth)) {
+            expiryErrorMsg.innerHTML = "";
+        } else if (
+            expiry[0].value.match(expMonth) &&
+            expiry[1].value.match(expYear)
+        ) {
+            expiryErrorMsg.innerHTML = "";
+        } else if (expiry[0] == "") {
+            expiryErrorMsg.innerHTML = "Can't be blank!";
+        } else {
+            expiryErrorMsg.innerHTML = "Wrong format!";
+        }
+    }
+    function validateCvc() {
+        let cvcErrorMsg = document.getElementById("cvc-error");
+        let cvcExp = /^[0-9]{3}$/;
+        if (cvc.value === "") {
+            cvcErrorMsg.innerHTML = "Can't be blank";
+        } else if (cvc.value.match(cvcExp)) {
+            cvcErrorMsg.innerHTML = "";
+        } else {
+            cvcErrorMsg.innerHTML = "Wrong format!";
+        }
+    }
+    validateCard();
+    validateName();
+    validateExpiry();
+    validateCvc();
+    if (
+        nameOnCard.innerHTML == cardholder.placeholder ||
+        numOnCard.innerHTML == cardNumber.placeholder ||
+        expMM.innerHTML == "00" ||
+        expYY.innerHTML == "0000" ||
+        cvcDisplay.innerHTML == "000" ||
+        (cardNumber.value.length > 0 && cardNumber.value.length < 16)
+    ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//submit button
+
+submit.addEventListener("click", event => {
+    massValidate();
+    if (massValidate() == false) {
+        event.preventDefault();
+    } else {
+        event.preventDefault();
+
+        form.classList.add("hidden");
+        thankYouSection.classList.remove("hidden");
+    }
+});
+
+// continue button
+
+continueBtn.addEventListener("click", event => {
+    event.preventDefault();
+    thankYouSection.classList.add("hidden");
+    form.classList.remove("hidden");
+    nameOnCard.innerHTML = cardholder.placeholder;
+    numOnCard.innerHTML = cardNumber.placeholder;
+    expMM.innerHTML = "00";
+    expYY.innerHTML = "0000";
+    cvcDisplay.innerHTML = "000";
+    cardholder.value = "";
+    cardNumber.value = "";
+    expiry[0].value = "";
+    expiry[1].value = "";
+    cvc.value = "";
+    expiryErrorMsg.innerHTML = "";
+});
